3fef41242018eb0926d0c253cf8093245e0bd685
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-01 15:32:34 -0300

    9-8_transaction
    Codigo refatorado e utilizando conceito de beginTransaction e commit

--- app/Services/CriadorDeSerie.php
+++ app/Services/CriadorDeSerie.php
@@ -4,26 +4,48 @@ namespace App\Services;
 
 use App\Serie;
 use http\Env\Request;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie
 {
     public function criarSerie(
         string $nomeSerie,
-        int $qtdTemporadas,
-        int $epPorTemporada
+        int    $qtdTemporadas,
+        int    $epPorTemporada
     ): Serie
     {
+        DB::beginTransaction();
         $serie = Serie::create(['nome' => $nomeSerie]);
+        $this->criaTemporada($qtdTemporadas, $serie, $epPorTemporada);
+        DB::commit();
+
+        return $serie;
+    }
+
+    /**
+     * @param int $qtdTemporadas
+     * @param $serie
+     * @param int $epPorTemporada
+     */
+    public function criaTemporada(int $qtdTemporadas, $serie, int $epPorTemporada): void
+    {
         $qtdTemporadas = $qtdTemporadas;
 
         for ($i = 1; $i <= $qtdTemporadas; $i++):
             $temporada = $serie->temporadas()->create(['numero' => $i]);
 
-            for($j = 1; $j <= $epPorTemporada; $j++):
-                $episodio = $temporada->episodios()->create(['numero' => $j] );
-            endfor;
+            $this->criaEpisodios($epPorTemporada, $temporada);
         endfor;
+    }
 
-        return $serie;
+    /**
+     * @param int $epPorTemporada
+     * @param $temporada
+     */
+    public function criaEpisodios(int $epPorTemporada, $temporada): void
+    {
+        for ($j = 1; $j <= $epPorTemporada; $j++):
+            $temporada->episodios()->create(['numero' => $j]);
+        endfor;
     }
 }
