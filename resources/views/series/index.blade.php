52fb6648da327c97d752d05e3613e582abca9b5b
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-01 16:09:02 -0300

    4-2_modificando para edição
    Utilizando também javascript para deixar mais dinamico

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -14,22 +14,53 @@
     <a href="{{ route('form_criar_serie') }}" class="btn btn-dark mb-2">Adicionar</a>
 
     <ul class="list-group">
-        @foreach ($series as $serie)
+        @foreach($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                {{$serie->nome}}
+                <span id="nome-serie-{{ $serie->id }}">{{ $serie->nome }}</span>
+
+                <div class="input-group w-50" hidden id="input-nome-serie-{{ $serie->id }}">
+                    <input type="text" class="form-control" value="{{ $serie->nome }}">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" onclick="editarSerie({{ $serie->id }})">
+                            Confirmar
+                        </button>
+                        @csrf
+                    </div>
+                </div>
 
                 <span class="d-flex">
-                    <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm mr-2">
-                        Visualizar
-                    </a>
-
-                    <form method="post" action="/series/{{$serie->id}}" onsubmit="return confirm('Tem certeza?')">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="Excluir" class="btn btn-danger btn-sm"/>
-                    </form>
-                </span>
+        <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{ $serie->id }})">
+            Editar
+        </button>
+        <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
+            Visualizar
+        </a>
+        <form method="post" action="/series/remover/{{ $serie->id }}"
+              onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($serie->nome) }}?')">
+            @csrf
+            @method('DELETE')
+            <button class="btn btn-danger btn-sm">
+                Deletar
+            </button>
+        </form>
+    </span>
             </li>
         @endforeach
     </ul>
+
+    <script>
+        function toggleInput(serieId) {
+            const nomeSerieEl = document.getElementById(`nome-serie-${serieId}`);
+            const inputSerieEl = document.getElementById(`input-nome-serie-${serieId}`);
+
+            if (nomeSerieEl.hasAttribute('hidden')) {
+                nomeSerieEl.removeAttribute('hidden');
+                inputSerieEl.hidden = true;
+            } else {
+                inputSerieEl.removeAttribute('hidden');
+                nomeSerieEl.hidden = true;
+            }
+
+        }
+    </script>
 @endsection
