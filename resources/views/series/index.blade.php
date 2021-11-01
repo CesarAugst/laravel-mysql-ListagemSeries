3e4c0a61c6216e60a5dd9424eb7d21aa2c98e482
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-01 13:47:58 -0300

    2-3_exibindo temporadas e episodios
    Feito busca no banco e com base no relacionamento monta uma listagem
    
    Novo controller e view adicionados apra exibir informações sobre temporadas

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -6,23 +6,30 @@
 
 @section('conteudo')
     @if(!empty($mensagem))
-    <div class="alert alert-success">
-        {{$mensagem}}
-    </div>
+        <div class="alert alert-success">
+            {{$mensagem}}
+        </div>
     @endif
 
     <a href="{{ route('form_criar_serie') }}" class="btn btn-dark mb-2">Adicionar</a>
 
     <ul class="list-group">
         @foreach ($series as $serie)
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            {{$serie->nome}}
-            <form method="post" action="/series/{{$serie->id}}" onsubmit="return confirm('Tem certeza?')">
-                @csrf
-                @method('DELETE')
-                <input type="submit" value="Excluir" class="btn btn-danger btn-sm"/>
-            </form>
-        </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                {{$serie->nome}}
+
+                <span class="d-flex">
+                    <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm mr-2">
+                        Visualizar
+                    </a>
+
+                    <form method="post" action="/series/{{$serie->id}}" onsubmit="return confirm('Tem certeza?')">
+                        @csrf
+                        @method('DELETE')
+                        <input type="submit" value="Excluir" class="btn btn-danger btn-sm"/>
+                    </form>
+                </span>
+            </li>
         @endforeach
     </ul>
 @endsection
