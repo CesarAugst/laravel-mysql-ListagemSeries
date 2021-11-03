ed7568e4909a6d22e7a32cf0fccdaaced2bb104c
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-03 14:56:31 -0300

    1-6_melhorando corpo do email
    As informações estão sendo passadas ao acessar o arquivo de rotas, ainda sem controller

--- resources/views/mail/serie/nova-serie.blade.php
+++ resources/views/mail/serie/nova-serie.blade.php
@@ -1,5 +1,8 @@
 <html>
 <body>
-Nova Série
+    <h1>Nova Série</h1>
+    Nome da Série: {{$nome}} <br>
+    Qtd Temporadas: {{$qtdTemporadas}} <br>
+    Qtd Episódios: {{$qtdEpisodios}}<br>
 </body>
 </html>
