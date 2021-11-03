d149e34fbbbc2651b0b4eeee486bbee9eedde358
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2021-11-03 16:23:34 -0300

    3-5_encaminhado emails para fila de processamento
    Agora não envia mais direto, encaminha para fila e depoois é enviado, porém ainda está manual

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -50,8 +50,8 @@ class SeriesController extends Controller
             );
             $email->subject = 'Nova Série Adicionada';
 
-            \Illuminate\Support\Facades\Mail::to($user)->send($email);
-            sleep(5);
+            \Illuminate\Support\Facades\Mail::to($user)->queue($email);
+            //sleep(5);
         }
 
         $request->session()
